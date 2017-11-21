1197656bb4f2582c5cef569679cdf3d1fd51b9fa
gahakuzhang <33209340+gahakuzhang@users.noreply.github.com>
2017-11-21 11:43:17 +0800

    Add files via upload

---
+++ 4.Working with lists/4.4.3 copying a list 复制列表-使用切片.py
@@ -0,0 +1,15 @@
+#4.4.3 copying a list 复制列表-使用切片
+my_idols=['nanamin','rentan','yuki']
+yc_idols=my_idols[:]
+my_idols.append('miona')
+yc_idols.append('kazumin')
+print('My favorite members are:')
+print(my_idols)
+print('\nyc\'s favorite members are:')
+print(yc_idols)
+
+
+
+
+
+
