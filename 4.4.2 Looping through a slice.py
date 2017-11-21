a6b556f232f912782eba09e36685d3ff53ed98c8
gahakuzhang <33209340+gahakuzhang@users.noreply.github.com>
2017-11-21 10:13:58 +0800

    Add files via upload

---
+++ 4.4.2 Looping through a slice.py
@@ -0,0 +1,5 @@
+#4.4.2 Looping through a slice 遍历切片
+idols=['nanamin','miona','nachan','maiyan','rentan','yuki']
+print('Here are my favorite three members in this group:')
+for idol in idols[:3]:
+    print(idol.title())
